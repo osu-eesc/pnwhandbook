ef98ef31325b9fd9b5a8c151c76c713cdcac2739
Bryan Mayjor <bmayjor@gmail.com>
2012-12-12 15:53:30 -0800

    initial commit

---
+++ template.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @file
+ * Template.php - process theme data for your sub-theme.
+ * 
+ * Rename each function and instance of "footheme" to match
+ * your subthemes name, e.g. if you name your theme "footheme" then the function
+ * name will be "footheme_preprocess_hook". Tip - you can search/replace
+ * on "footheme".
+ */
+
+
+/**
+ * Override or insert variables for the html template.
+ */
+/* -- Delete this line if you want to use this function
+function pnwhandbook_preprocess_html(&$vars) {
+}
+function pnwhandbook_process_html(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables for the page templates.
+ */
+/* -- Delete this line if you want to use these functions
+function pnwhandbook_preprocess_page(&$vars) {
+}
+function pnwhandbook_process_page(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the node templates.
+ */
+/* -- Delete this line if you want to use these functions
+function pnwhandbook_preprocess_node(&$vars) {
+}
+function pnwhandbook_process_node(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the comment templates.
+ */
+/* -- Delete this line if you want to use these functions
+function pnwhandbook_preprocess_comment(&$vars) {
+}
+function pnwhandbook_process_comment(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the block templates.
+ */
+/* -- Delete this line if you want to use these functions
+function pnwhandbook_preprocess_block(&$vars) {
+}
+function pnwhandbook_process_block(&$vars) {
+}
+// */
